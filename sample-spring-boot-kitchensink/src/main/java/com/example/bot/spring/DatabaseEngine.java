2ab24f981a35feed4553c2f6a389b1f9bf24d54c
Chung Wa KO <gordon.cw.ko@gmail.com>
2017-09-17 19:21:10 +0800

    Test #2

--- sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
+++ sample-spring-boot-kitchensink/src/main/java/com/example/bot/spring/DatabaseEngine.java
@@ -36,7 +36,10 @@ public class DatabaseEngine {
 			
 			while (result == null && (sCurrentLine = br.readLine()) != null) {
 				String[] parts = sCurrentLine.split(":");
-				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+//				if (text.toLowerCase().equals(parts[0].toLowerCase())) {
+//					result = parts[1];
+//				}
+				if (text.toLowerCase().contains(parts[0].toLowerCase())) {
 					result = parts[1];
 				}
 			}
